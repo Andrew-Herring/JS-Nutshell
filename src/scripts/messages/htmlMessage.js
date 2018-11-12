a60f466cdb2d3452f43ed1697591f952c486c6d9
John Wood <johnnathanwood@gmail.com>
2018-11-12 14:16:45 -0600

    datestamp working on message output

--- src/scripts/messages/htmlMessage.js
+++ src/scripts/messages/htmlMessage.js
@@ -3,7 +3,7 @@ const htmlMessage = (message) => {
     return `
     <div>
     <p>${message.content}</p>
-    <p${message.date}></p>
+    <p>${message.date}</p>
     <button id="delete!${message.id}">Delete</button>
     <button id="edit!${message.id}">Edit</button>
     </div>`
