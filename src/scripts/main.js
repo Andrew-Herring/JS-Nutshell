4b7b62988d3fc7b485b42ed75a3e68745cb4821c
buffard <samuelwebber19@gmail.com>
2018-11-12 09:31:06 -0600

    edit file structure

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-import sayHello from "./hello"
-import sayGoodbye from "./goodbye"
-import SandwichMaker from "./sandwichMaker"
-
-sayHello()
-sayGoodbye()
-
-SandwichMaker.placeOrder("rye", "capicola", "provolone")
