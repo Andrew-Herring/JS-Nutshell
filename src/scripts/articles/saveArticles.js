1b9067c53479a7ee634722e1b160a960bee9fc8e
rjlancaster <rjlancaster@gmail.com>
2018-11-13 17:34:16 -0600

    completed comments for articles module

--- src/scripts/articles/saveArticles.js
+++ src/scripts/articles/saveArticles.js
@@ -4,7 +4,7 @@ import { articlesFormManager } from "./articlesFormManager"
 import { articlesDomRender } from "./articlesDomRender"
 import { timeStamp } from "../date/timeStamp"
 
-//The saveArticles function contains functionality for saving form values to JSON.  The activeUser is passed in through an argument so that the user ID is passed into the article entry.
+//The saveArticles function contains functionality for saving form values to JSON.  activeUser is passed in through an argument so that the acting user ID is passed into the article entry.
 const saveArticles = (activeUser) => {
   // click event looks at the articlesInput div and adds a click event to the single button we have in that div
   document.querySelector("#articlesInput").addEventListener("click", (evt) => {
@@ -13,10 +13,12 @@ const saveArticles = (activeUser) => {
       title: document.querySelector("#articlesTitle").value,
       url: document.querySelector("#articlesUrl").value,
       synopsis: document.querySelector("#articlesSynopsis").value,
-      timestamp: timeStamp()
+      timestamp: timeStamp(),
+      completed: false
     }
-    // If any input in the form is not filled out, we return an error
+    // Our one button within the articlesInput div serves two functions: to save a new entry and to edit an existing entry.  We use an if/else statement to differentiate between the two functions.  First, we look to see if the id of the button includes "save."
     if (evt.target.id.startsWith("saveArticles")) {
+      // If any input in the form is not filled out, we return an error
       if (!entry.title || !entry.url || !entry.synopsis) {
         alert("Please fill out the article")
       } else {
@@ -27,8 +29,11 @@ const saveArticles = (activeUser) => {
         })
       }
     }
+    // If our button id starts with "edit,"...
     if (evt.target.id.startsWith("editArticles")) {
+    // we split the id assigned to the button to an array. The second entry of the array will contain the targeted unique entry id.  The unique id is passed to the id variable.
       const id = evt.target.id.split("!")[1]
+    // We call the editEntry function from data manager and pass in the unique id. editEntry contains a PUT method, therefore editing the existing record.  Once the promise is fulfilled, we clear the form, render the DOM for the active user and finally change the id assigned to our button back to start with "save."
       articlesDataManager.editEntry(entry, id).then(() => {
         articlesFormManager.clearForm()
         articlesDomRender(activeUser)
@@ -37,5 +42,5 @@ const saveArticles = (activeUser) => {
     }
   })
 }
-
+// The saveArticles function is exported for use in other functions.
 export { saveArticles }
