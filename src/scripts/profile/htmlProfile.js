c1fa372ccf4e546c50d77186e947f9c97d263e7d
Andrew Herring <andrewoherring@gmail.com>
2018-11-14 11:54:23 -0600

    set favorite button toggles favorite status

--- src/scripts/profile/htmlProfile.js
+++ src/scripts/profile/htmlProfile.js
@@ -3,13 +3,14 @@ const htmlProfile = (entry) => {
   return `
   <div>
   <h2>${entry.contact}</h2>
+  <h3>${entry.favorite}</h3>
   <p>${entry.phone}</p>
   <p>${entry.city}</p>
   <p>${entry.about}</p>
   <p>${entry.secret}</p>
   <button id="deleteProfile!${entry.id}">Delete Person</button>
   <button id="editProfile!${entry.id}">Edit Details</button>
-  <button id="favBtn!${entry.id}">Favorite</button>
+  <button id="favBtn!${entry.id}">Set Favorite</button>
   </div>`
 }
 
