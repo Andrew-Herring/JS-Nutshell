4c143213ded5ec6d2d1bb3108987a148654b18ea
buffard <samuelwebber19@gmail.com>
2018-11-14 16:00:17 -0600

    working on bootstrap

--- src/scripts/tasks/completedTaskHtmlEntry.js
+++ src/scripts/tasks/completedTaskHtmlEntry.js
@@ -1,14 +1,16 @@
 //representation of completed DOM entries
 const completedTaskHtmlEntry = (entry) => {
   return `
-  <div class="card">
+  <div class="card w-50 text-white bg-success mb-3">
     <h3 class="card-header">${entry.task}</h3>
-    <p>${entry.description}</p>
-    <p>${entry.dueDate}</p>
-    <button id="delete!${entry.id}">Delete</button>
-    <button id="editTask!${entry.id}">Edit</button>
+    <div class="card-body">
+    <p class="card-text">${entry.description}</p>
+    <p class="card-text">${entry.dueDate}</p>
+    <button id="delete!${entry.id}" class="btn btn-primary">Delete</button>
+    <button id="editTask!${entry.id}" class="btn btn-primary">Edit</button>
     <input type="checkbox" name="completedTask" id="completeTask!${entry.id}"> 
     <label id="checkboxLabel" for="completeTask!${entry.id}">Redo Task</label>
+  </div>  
   </div>`
 }
 
--- src/scripts/tasks/taskHtmlEntry.js
+++ src/scripts/tasks/taskHtmlEntry.js
@@ -1,14 +1,16 @@
 //representation of DOM entries
 const taskHtmlEntry = (entry) => {
   return `
-  <div>
-    <h3>${entry.task}</h3>
-    <p>${entry.description}</p>
-    <p>${entry.dueDate}</p>
-    <button id="delete!${entry.id}">Delete</button>
-    <button id="editTask!${entry.id}">Edit</button>
+  <div class="card w-50 text-white bg-warning mb-3">
+    <h3 class="card-header">${entry.task}</h3>
+    <div class="card-body">
+    <p class="card-text">${entry.description}</p>
+    <p class="card-text">${entry.dueDate}</p>
+    <button id="delete!${entry.id}" class="btn btn-primary">Delete</button>
+    <button id="editTask!${entry.id}" class="btn btn-primary">Edit</button>
     <input type="checkbox" name="completedTask" id="completeTask!${entry.id}"> 
-    <label id="checkboxLabel" for="completeTask!${entry.id}">All Done</label>
+    <label id="checkboxLabel" for="completeTask!${entry.id}">Redo Task</label>
+  </div>  
   </div>`
 }
 
