f28da4c73708f3e8b4a083f7546fa0e279de3f7a
buffard <samuelwebber19@gmail.com>
2018-11-14 20:55:51 -0600

    added styling to fully working task app

--- src/scripts/tasks/completedTaskHtmlEntry.js
+++ src/scripts/tasks/completedTaskHtmlEntry.js
@@ -1,13 +1,13 @@
 //representation of completed DOM entries
 const completedTaskHtmlEntry = (entry) => {
   return `
-  <div class="card w-50 text-white bg-success mb-3">
+  <div class="card text-white bg-success mb-3 shadow">
     <h3 class="card-header">${entry.task}</h3>
     <div class="card-body">
     <p class="card-text">${entry.description}</p>
     <p class="card-text">${entry.dueDate}</p>
-    <button id="delete!${entry.id}" class="btn btn-primary">Delete</button>
-    <button id="editTask!${entry.id}" class="btn btn-primary">Edit</button>
+    <button id="delete!${entry.id}" class="btn btn-danger btn-sm">Delete</button>
+    <button id="editTask!${entry.id}" class="btn btn-primary btn-sm">Edit</button>
     <input type="checkbox" name="completedTask" id="completeTask!${entry.id}"> 
     <label id="checkboxLabel" for="completeTask!${entry.id}">Redo Task</label>
   </div>  
--- src/scripts/tasks/tasksFormManager.js
+++ src/scripts/tasks/tasksFormManager.js
@@ -2,16 +2,26 @@ const tasksFormManager = {
   //this is our form that the user will interact with
   tasksForm: () => {
     return `
-    <div>
-      <h2>My Task List</h2>
-      Task:
-      <input type="text" name="Task" id="taskName">
-      Description:
-      <input type="textarea" name="Description" id="description">
-      Due Date:
-      <input type="date" name="Date" id="date">
-      <button id="taskSaveBtn">Save</button>
-    </div>
+    <div class="card p-3 mb-2 bg-light text-dark">
+      <div class="card-body">
+        <h2>My Task List</h2>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="taskName">Task</label>
+              <input type="text" name="Task" class="form-control" id="taskName" placeholder="Add a task">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="date">Due Date</label>
+              <input type="date" name="Date" class="form-control" id="date">
+            </div>
+            <div class="form-group w-100 p-3">
+              <label for="description">Description</label>
+              <input type="text" name="Description" class="form-control " id="description" placeholder="Make sure to...">
+            </div>
+          </div>
+          <button class="btn btn-primary" id="taskSaveBtn">Save</button>
+      </div>
+    </div>    
     `
   },
   //will clear the form  after the user has submitted a task
